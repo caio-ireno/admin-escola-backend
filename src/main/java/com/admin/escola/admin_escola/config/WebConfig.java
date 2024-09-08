a460580e9b2b121f35c7a2a5bb62a434f2e2a63a
caio-ireno <caio_ireno@hotmail.com>
2024-09-08 16:37:35 -0300

    Feat: web config - CORS - deploy env.

--- src/main/java/com/admin/escola/admin_escola/config/WebConfig.java
+++ src/main/java/com/admin/escola/admin_escola/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://admin-escola-frontend.netlify.app") // URL do frontend local
+                .allowedOrigins("https://admin-escola-frontend.netlify.app/") // URL do frontend local
                 //.allowedOrigins("http://localhost:5173/") // URL do frontend local
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
